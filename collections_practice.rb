e7dabc0ea78f9ae98310e9c6a1d0ea456532bf9f
Joseph Perez <hello@josephdanielperez.com>
2020-07-01 03:39:21 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,19 @@
+def sort_array_asc(array)
+  array.sort do |asc|
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |desc|
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |char|
+  end
+end
+
+def swap_elements(array)
+  array.sort do |elm|
+  end
+end
